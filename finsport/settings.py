5b4fa306e7ad51b27df9ad063ee198aea5344c0b
Luis Jarufe <luisjarufe@gmail.com>
2020-07-10 16:01:43 -0500

    mail configuration

--- finsport/settings.py
+++ finsport/settings.py
@@ -128,13 +128,13 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_USE_TLS = True
 
-EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST = env('EMAIL_HOST', default='localhost')
 
-EMAIL_PORT = 587
+EMAIL_PORT = env('EMAIL_PORT', default=25)
 
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='')
 
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='')
 
 INSTANCE_DOMAIN = env('INSTANCE_DOMAIN')
 
